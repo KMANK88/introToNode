d9c7cc745bea538502d9d9f2d708b8badb2973a3
Karsten Pease <karstenpease88@outlook.com>
2017-06-29 11:13:00 -0600

    added date creator

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
-var port = 2000;
+//creating your own modual you have to be path specific
+var makeDate = require('./dateMaker');
+console.log(makeDate(10));
+var port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,9 +14,9 @@ app.get('/instagrams',function(req,res){
 });
 
 app.post('/instagrams',function(req,res){
-  res.send("Take off your pants and your pannies");
+  res.send("From port");
 })
 
 var server = app.listen(port,function(){
-  console.log("Running on port 2000");
+  console.log("Running on port 3000");
 });
